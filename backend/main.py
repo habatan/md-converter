de154213fde8abbdf2bab5a0a71a1e4992e8b376
habatan <ryoutarou_ha@icloud.com>
2026-09-22 19:54:51 +0900

    fix: backend 環境変数取得部分修正
    アクセス設定における環境変数のurl部分における"/"をつけずにデプロイする必要あり

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # 環境変数からフロントエンドのURLを取得（未設定の場合はローカル環境をデフォルトに）
+# 末尾の/がCORSエラーの原因になるため、rstrip("/")で自動的に削除します
 frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173")
-
 # フロントエンドからの通信を許可する設定（CORS）
 app.add_middleware(
     CORSMiddleware,
